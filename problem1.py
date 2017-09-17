7c6d5be074a310b8674b812a08653bbf40a93569
Mikal Stapnes <mstapnes@gmail.com>
2017-09-17 19:03:00 +0200

    Restructured problem 1

--- problem1.py
+++ problem1.py
@@ -3,38 +3,102 @@ import matplotlib.pyplot as plt
 import numpy as np
 from problem1plots import plot
 
-N = 30
 
-def p_fixed_n(k):
-    res = 0
+def p_fixed_n(k, N):
+    res = k / N
     for i in range(k + 1, N + 1):
         res += 1 / (i - 1)
-    res *= (k / N)
     return res
 
-def simulate(k):
-    N = round(np.random.uniform(16, 45))
-    choise = N - 1
-    best = [0, 0, 0]
-    best_k = 0
-    candidates = []
-    for i in range(0, N):
-        c = np.random.uniform(1, 1000)
-        candidates.append(c)
-        if (best[0] <= c):
-            best[2] = best[1]
-            best[1] = best[0]
-            best[0] = c
-        if i < k:
-            best_k = max(best_k, c)
-    for i in range(k, N):
-        if candidates[i] > best_k:
-            choise = i
+
+def secretary_problem(k, candidates):
+    """f(k, X) -> {b_1, b_2, b_3}
+    
+    Runs an instance of the secretary problem on the
+    candidates, and returns an array with three boolean values: 
+        -> b_1, Whether the algorithm successfully picks the largest value
+        -> b_2, Whether the algorithm successfully picks a value
+                among the top 3
+        -> b_3, Whether the algorithm interviews all candidates
+    """
+    
+    b_1 = 0
+    b_2 = 0
+    b_3 = 0
+    
+    # Sample the first k candidates
+    x_star = max(candidates[0:k])
+    
+    # Run the algorithm
+    x_chosen = None
+    for j, cand in enumerate(candidates[k:-2]):
+        if (cand > x_star):
+            x_chosen = cand
             break
-    if candidates[choise] == best[0]:
-        return 1
-    else:
-        return 0
+    # If we have not yet chosen a candidate we are forced to 
+    # choose the last
+    if (x_chosen == None):
+        x_chosen = candidates[-1]
+        b_3 = 1
+    
+    # Sort the candidate array and count backwards
+    # until the chosen value is found
+    candidates = np.sort(candidates)[::-1]
+    n = 0
+    for cand in candidates:
+        n += 1
+        if (cand == x_chosen):
+            break    
+    if n==1:
+        b_1 = 1
+    if n<=3:
+        b_2 = 1
+    
+    return [b_1, b_2, b_3]
+
+
+def simulate_const_n(k, N, iterations):
+    """f(k, N, iterations) -> [r_best, r_top_three, r_interview_all]
+    
+    Runs iterations of the secretary problem with N candidates where the k first
+    are observed. N can either be an fixed or an interval, in which N is chosen 
+    randomly uniform in the interval. Returns an array with three floats
+    between 0 and 1:
+        -> r_best, the ratio of times the algorithm returned the best candidate
+        -> r_top_three, the ratio of times the algorithm returned a
+            candidate among the top three.
+        -> r_interview_all, the ratio of times the algorithm interviewed all 
+            candidates and subsequently chose the last candidate.
+    """
+    r_best = 0
+    r_top_three = 0
+    r_interview_all = 0
+    
+    # Checks wether we want a fixed or dynamic number of candidates
+    fixed = True
+    if not isinstance(N, int):
+        fixed=False    
+
+    for _ in range(iterations):
+        
+        # If N is not fixed, choose it randomly uniform in the interval
+        if not fixed:
+            N_candidates = np.random.randint(N[0], N[1])
+        else:
+            N_candidates = N
+        
+        candidates = np.random.randint(0, 100, N_candidates)
+        result = secretary_problem(k, candidates)
+        r_best += result[0]
+        r_top_three += result[1]
+        r_interview_all += result[2]
+    
+    r_best /= iterations
+    r_top_three /= iterations
+    r_interview_all /= iterations
+        
+    return [r_best, r_top_three, r_interview_all]
+          
 
 def p(k):
     res = 0
@@ -49,17 +113,8 @@ def p(k):
     return res
 
 def main():
-    """
-    kvalues = [ i for i in range(1, 30 + 1) ]
-    pvalues = [ p_fixed_n(k) for k in kvalues ]
-    plot(kvalues, pvalues, "Probabilty of choosing best candidate for fixed N = 30",
-         "$k$", "$P(Z = 1 | K = k)$", 30 / np.e, 1, 31, 0, 0.5)
-    """
+    print(simulate_const_n(11, 30, 1000))
     
-    kvalues = [ i for i in range(1, 16) ]
-    pvalues = [ p(k) for k in kvalues ]
-    plot(kvalues, pvalues, "$P(Z = 1)$, given uniformly distrbuten candidate size", "$k$", "$p$",
-         10, 1, 15, 0, 0.5)
     
 
 if __name__ == "__main__":
