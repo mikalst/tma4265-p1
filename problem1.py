2c44477a133f894c3d5af9e2cee0c696147304d7
Øyvind Auestad <oyvind.auestad@hotmail.com>
2017-09-18 21:53:21 +0200

    oppdatert

--- problem1.py
+++ problem1.py
@@ -1,24 +1,37 @@
 # -*- coding: utf-8 -*-
-import matplotlib.pyplot as plt
 import numpy as np
-from problem1plots import plot
+from problem1plots import plot1, plot2
 
-N = 30
-
-def p_fixed_n(k):
+def p_fixed_n(n, k):
+    #Probability of choosing the best candidate with 
+    #N fixed = n
     res = 0
-    for i in range(k + 1, N + 1):
+    for i in range(k + 1, n + 1):
         res += 1 / (i - 1)
-    res *= (k / N)
+    res *= (k / n)
+    return res
+
+def p(k):
+    #Probability of choosing the best candidate with N 
+    #uniformly distributed in { 16, 17, ..., 45 }
+    res = 0
+    for i in range(16, 46):
+        res += p_fixed_n(i, k);
+    res /= 30            
     return res
 
 def simulate(k):
-    N = round(np.random.uniform(16, 45))
-    choise = N - 1
+    #A random integer between 16 and 45 is chosen uniformly
+    n = round(np.random.uniform(15.5, 45.49))
+    choise = n - 1
+    #list holding the top three candidates
     best = [0, 0, 0]
+    #the best candidate among the first k
     best_k = 0
     candidates = []
-    for i in range(0, N):
+    for i in range(0, n):
+        #a candidate's value is uniformly distributed between
+        #1 and 1000
         c = np.random.uniform(1, 1000)
         candidates.append(c)
         if (best[0] <= c):
@@ -27,40 +40,46 @@ def simulate(k):
             best[0] = c
         if i < k:
             best_k = max(best_k, c)
-    for i in range(k, N):
+            
+    for i in range(k, n):
         if candidates[i] > best_k:
+            #the candidate is finally chosen
+            #accoring to the algorithm
             choise = i
             break
+        
     if candidates[choise] == best[0]:
         return 1
     else:
         return 0
-
-def p(k):
-    res = 0
-    for i in range(16, 46):
-        _res = 0
-        for j in range(k + 1, i + 1):
-            _res += 1 / (j - 1)
-            
-        _res *= k / i
-        res += _res
-    res /= 30            
-    return res
+    
+def run_simulations(n, k):
+    successes = 0
+    for i in range(n):
+        successes += simulate(k)
+    return successes / n
 
 def main():
-    """
-    kvalues = [ i for i in range(1, 30 + 1) ]
-    pvalues = [ p_fixed_n(k) for k in kvalues ]
-    plot(kvalues, pvalues, "Probabilty of choosing best candidate for fixed N = 30",
-         "$k$", "$P(Z = 1 | K = k)$", 30 / np.e, 1, 31, 0, 0.5)
-    """
     
+    #plotting in b)
+    kvalues1 = [ i for i in range(1, 31) ]
+    pvalues1 = [ p_fixed_n(30, k) for k in kvalues1 ]
+    kvalues2 = [ i for i in range(1, 41) ]
+    pvalues2 = [ p_fixed_n(40, k) for k in kvalues2 ]
+    plot2(kvalues1, pvalues1, kvalues2, pvalues2, "$P(Z = 1)$ for fixed $N = 30, 40$",
+         "$k$", "$p$", 30 / np.e, 40 / np.e, 1, 40, 0, 0.5)
+    
+    #plotting in d)
     kvalues = [ i for i in range(1, 16) ]
     pvalues = [ p(k) for k in kvalues ]
-    plot(kvalues, pvalues, "$P(Z = 1)$, given uniformly distrbuten candidate size", "$k$", "$p$",
+    plot1(kvalues, pvalues, "$P(Z = 1)$ with uniformly distributed candidate size", "$k$", "$p$",
          10, 1, 15, 0, 0.5)
     
+    #simulate successes in chosing the best candidate in task d)
+    result = run_simulations(10000, 10)
+    print(result)
+    #a run gives 0.36265, which is very close to the actual probabily, 0.3617
+
 
 if __name__ == "__main__":
     main()
--- problem1plots.py
+++ problem1plots.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 
-def plot(xvalues, yvalues, title, xlabel, ylabel, opt, xl, xu, yl, yu):
-    
-    fig = plt.figure()
+titlesize = 18
+textsize = 12.5
+
+def plot1(xvalues, yvalues, title, xlabel, ylabel, opt, xl,
+          xu, yl, yu):
+    plt.figure()
     plt.style.use("ggplot")
     plt.grid(True)
-    
-    plt.title(title)
+    plt.title(title, fontsize = titlesize)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.xlim(xl, xu)
@@ -15,12 +17,51 @@ def plot(xvalues, yvalues, title, xlabel, ylabel, opt, xl, xu, yl, yu):
     
     xopt = round(opt)
     yopt = yvalues[xopt - 1]
-    
-    plt.annotate("optimal choise,\n({}, {})".format(xopt, round(yopt, 4)), xy = (xopt, yopt),
-                 xytext = (xopt + 1, yopt + 0.05), fontsize = 20,
+
+    plt.annotate("Optimal choise,\nk = {},\np = {}".format(xopt, round(yopt, 4)), xy = (xopt, yopt),
+                 xytext = (xopt + 0.5, yopt + 0.05), fontsize = 12.5, color = "red",
                  arrowprops = dict(facecolor = "black", shrink = 0.05))
     
-    plt.plot(xvalues, yvalues)
-    
+    plt.plot(xvalues, yvalues, "ro", color = "red")
+    plt.plot(xvalues, yvalues, "b", color = "red")
     plt.show()
+
+def plot2(xvalues1, yvalues1, xvalues2, yvalues2, title,
+          xlabel, ylabel, opt1, opt2, xl, xu, yl, yu):
+    plt.figure()
+    plt.style.use("ggplot")
+    plt.title(title, fontsize = titlesize)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.xlim(xl, xu)
+    plt.ylim(yl, yu)
+    
+    xopt1 = round(opt1)
+    xopt2 = round(opt2)
+    yopt1 = yvalues1[xopt1 - 1]
+    yopt2 = yvalues2[xopt2 - 1]
+    plt.annotate("$N = 30$", xy = (34, 0.45), fontsize = 17, color = "red")
+    plt.annotate("$N = 40$", xy = (34, 0.40), fontsize = 17, color = "blue")
+    plt.annotate("Optimal choise,\nk = {},\np = {}".format(xopt1, round(yopt1, 4)), xy = (xopt1, yopt1),
+                                             xytext = (xopt1 - 8.5, yopt1 + 0.04), fontsize = textsize, color = "red",
+                                             arrowprops = dict(facecolor = "black", shrink = 0.05) )
+    plt.annotate("Optimal choise,\nk = {},\np = {}".format(xopt2, round(yopt2, 4)), xy = (xopt2, yopt2),
+                                             xytext = (xopt2, yopt1 + 0.04), fontsize = textsize, color = "blue",
+                                             arrowprops = dict(facecolor = "black", shrink = 0.05) )
+    
+    
+    plt.plot(xvalues1, yvalues1, "ro", color = "red")
+    plt.plot(xvalues1, yvalues1, "b", color = "red")
+    plt.plot(xvalues2, yvalues2, "ro", color = "blue")
+    plt.plot(xvalues2, yvalues2, "b", color = "blue")
+    plt.show()
+
+
+
+
+
+
+
+
+
 
