3cd10fd178aa162be6e914a2af9127170c00e2c5
Mikal Stapnes <mstapnes@gmail.com>
2017-09-18 10:23:39 +0200

    Completed code, now starting plotting

--- problem1.py
+++ problem1.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
-from problem1plots import plot
-
-
-def p_fixed_n(k, N):
-    res = k / N
-    for i in range(k + 1, N + 1):
-        res += 1 / (i - 1)
-    return res
 
 
 def secretary_problem(k, candidates):
@@ -26,32 +18,30 @@ def secretary_problem(k, candidates):
     b_2 = 0
     b_3 = 0
     
+    # if k > N one will always interview all candidates with no chance
+    if (k>len(candidates)):
+        return [0, 0, 1]
+    
     # Sample the first k candidates
     x_star = max(candidates[0:k])
     
-    # Run the algorithm
+    # Run the choice algorithm
     x_chosen = None
-    for j, cand in enumerate(candidates[k:-2]):
+    for cand in candidates[k:-1]:
         if (cand > x_star):
             x_chosen = cand
             break
-    # If we have not yet chosen a candidate we are forced to 
-    # choose the last
     if (x_chosen == None):
         x_chosen = candidates[-1]
         b_3 = 1
     
-    # Sort the candidate array and count backwards
-    # until the chosen value is found
-    candidates = np.sort(candidates)[::-1]
-    n = 0
-    for cand in candidates:
-        n += 1
-        if (cand == x_chosen):
-            break    
-    if n==1:
+    # Sort the candidate array
+    candidates = np.sort(candidates)
+        
+    # Check if chosen value is best or top 3
+    if (x_chosen == candidates[-1]):
         b_1 = 1
-    if n<=3:
+    if (x_chosen in candidates[-3:]):
         b_2 = 1
     
     return [b_1, b_2, b_3]
@@ -70,6 +60,7 @@ def simulate_const_n(k, N, iterations):
         -> r_interview_all, the ratio of times the algorithm interviewed all 
             candidates and subsequently chose the last candidate.
     """
+    
     r_best = 0
     r_top_three = 0
     r_interview_all = 0
@@ -81,13 +72,13 @@ def simulate_const_n(k, N, iterations):
 
     for _ in range(iterations):
         
-        # If N is not fixed, choose it randomly uniform in the interval
+        # If N is not fixed, choose it randomly uniform in the closed interval
         if not fixed:
-            N_candidates = np.random.randint(N[0], N[1])
+            N_candidates = np.random.randint(N[0], N[1]+1)
         else:
             N_candidates = N
         
-        candidates = np.random.randint(0, 100, N_candidates)
+        candidates = np.random.randint(0, 1e9, N_candidates)
         result = secretary_problem(k, candidates)
         r_best += result[0]
         r_top_three += result[1]
@@ -100,20 +91,42 @@ def simulate_const_n(k, N, iterations):
     return [r_best, r_top_three, r_interview_all]
           
 
-def p(k):
+def p_fixed_n(k, N):
     res = 0
-    for i in range(16, 46):
-        _res = 0
-        for j in range(k + 1, i + 1):
-            _res += 1 / (j - 1)
-            
-        _res *= k / i
-        res += _res
-    res /= 30            
+    for i in range(k+1, N + 1):
+        res += 1 / (i - 1)
+    res *= k / N
     return res
 
+
+def p_varying_n(k):
+    res = 0
+    for N in range(16, 45 + 1):
+        res += p_fixed_n(k, N)
+    res /= 30
+    return res
+
+
+def plot_analytical():
+    k_1 = range(1,30)
+    k_2 = range(1, 40)
+    y_1 = [p_fixed_n(k, 30) for k in k_1]
+    y_2 = [p_fixed_n(k, 40) for k in k_2]
+    
+    
+    plt.style.use('ggplot')
+    plt.plot(k_1, y_1)
+    plt.plot(k_2, y_2)
+
+
 def main():
-    print(simulate_const_n(11, 30, 1000))
+    flag = {0: "1b",
+            1: "1c",
+            2: "1d"}[0]
+    
+    if flag == "1b":
+        plot_analytical()
+    
     
     
 
