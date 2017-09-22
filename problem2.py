77f862a764c7f0306bd0e9cde0734afe0aa9d3ce
Mikal Stapnes <mstapnes@gmail.com>
2017-09-22 19:56:56 +0200

    Fix task e; Separate into math functions and plotting functions

--- problem2.py
+++ problem2.py
@@ -4,62 +4,24 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 plt.style.use('ggplot') #to be used for pthe plot in task a
 
-#Making transition matrix
-P = np.matrix([[0.95, 0.05], [0, 1]])
-#initialized vector
-x = np.matrix([[0.99, 0.01]])
+P = np.matrix([[0.95, 0.05], [0.0, 1.0]])
 
-
-def marginal_prob(k):
+def P_x(k):
+    """Returns the probability of X_k = 1
+    """
     if (k == 1):
         return 0.99
     else:
         return 0.99 * 0.95**(k-1)
 
-#a
-def task_a(P, x):
-    '''
-    Doing 50 iterations and storing the probability for class 2
-    '''
-    #list to append probabilities and appending the first probability
-    prob_1 = [0.99]
-    prob_2 = [0.01]
-    
-    
-
-    #doing 50 iterations
-    for k in range (1, 50):
-        v = x*(P**k)
-        prob_1.append(v[0, 0])
-        prob_2.append(v[0, 1])
-        
-    return prob_1, prob_2
-
-def plot_a(P,x):
-    '''
-    plotting the probability as a funtion of n
-    '''
-    probabilities = task_a(P, x)
-    #plotting marginal probabilities
-    plt.figure()
-    plt.plot(range(1,51), probabilities[::2]) #flattened list
-    plt.title('Marginal probabilities for $P(X_n = 1)$ as function of $n$.', fontsize=14)
-    plt.xlabel('$n$')
-    plt.ylabel('$P(X_n = 1)$')
-    plt.show()
-
-#plot_a(P,x)
-
-#b
-def task_b_simulate_hill(n_sim, P, x):
+def simulate_hill(n_sim):
     '''
     The problem is identical to a geometric distribution
     and is programmed in that way. A list of markov chains is returned
     '''
     #list to store each simulation
     realizations = []
-    count = 0 # debugging
-    #simulating
+
     for _ in range(n_sim):
         #setting all roads to risk 1
         x = np.ones((50,), dtype=np.int)
@@ -91,13 +53,83 @@ def task_b_simulate_hill(n_sim, P, x):
     #returning the list of the simulations
     return realizations, means
 
-#task_b_simulate_hill(50, P, x)
+def P_bw(k):
 
-def plot_b(P, x):
+    PB = np.matrix([[1.0,0.0],[0.0,0.0]])
+    PB[1,0]=P[0,1]*P_x(k-1)/(1-P_x(k))
+    PB[1,1]=P[1,1]*(1-P_x(k-1))/(1-P_x(k))
+    return PB
+
+def sensor_prob(k):
+    """
+    Compute forward and backward probability
+    """
+    # Set state vectors
+    v10 = np.matrix([1,0])
+    v11 = np.matrix([1,0])
+    v20 = np.matrix([0,1])
+    v21 = np.matrix([0,1])
+ 
+    y1 = np.zeros((50,))
+    y2 = np.zeros((50,))
+    
+    lim = 50
+    
+    plac = k
+    while(plac > 1):
+        v10 = v10 * P_bw(plac)
+        v20 = v20 * P_bw(plac)
+        y1[plac-2] = v10[0, 1]
+        y2[plac-2] = v20[0, 1]
+        plac -= 1
+    plac = k-1
+    while(plac < lim):
+        y1[plac] = v11[0, 1]
+        y2[plac] = v21[0, 1]
+        v11 = v11 * P
+        v21 = v21 * P
+        plac += 1
+
+    return y1, y2
+
+
+def information_gain(k):
+    y1, y2 = sensor_prob(k)
+    
+    s1 = 5000*sum(y1)
+    s2 = 5000*sum(y2)
+        
+    total = min([100000, s1])*P_x(k) + min([100000, s2])*(1-P_x(k))
+
+    return total
+
+
+def plot_c(k):
+    y1, y2 = sensor_prob(k)
+    print('Length og y1 and y2: ', len(y1), ', ', len(y2))
+    plt.style.use("ggplot")
+    plt.title('Forward and backward propabilities as a function of $l$')
+    plt.ylabel("$P(X_l = 2)$")
+    plt.xlabel("$l$")
+    plt.plot(range(1,51), y1)
+    plt.plot(range(1,51), y2)
+    plt.legend(["$P(X_l = 2 | X_{20} = 1)$", "$P(X_l = 2 | X_{20} = 2)$"])
+    plt.show()
+
+
+def task_a():
+    plt.figure()
+    plt.title('Marginal probabilities for $P(X_n = 1)$ as function of $n$.', fontsize=14)
+    plt.xlabel('$n$')
+    plt.ylabel('$P(X_n = 1)$')
+    plt.plot(range(1,51), [P_x(k) for k in range(1,51)])
+
+
+def task_b():
     '''
     plotting the realizations from task 2b with imshow
     '''
-    image, means = task_b_simulate_hill(25, P, x)
+    image, means = simulate_hill(25)
 
     plt.figure()
     ax = plt.gca() #to size the colorbar
@@ -119,103 +151,33 @@ def plot_b(P, x):
     plt.xlabel('$n$')
     plt.ylabel('Average')
     plt.show()
+    
 
-#plot_b(P, x)
-
-def task_c(P, x, k):
-    '''
-    computing forward and backward probability
-    '''
-    fw = np.matrix([[0.95, 0.05], [0, 1]])
-    bw = np.matrix([[1, 0], [0.05, 0.95]])
-
-    v1 = [1,0]
-    v2 = [0,1]
-    y1 = []
-    y2 = []
-
-    for l in range(k-1,0, -1):
-        y1.append((v1*bw**l)[0,1])
-        y2.append((v2*bw**l)[0,1])
-
-    for l in range(0, 51-k):
-        y1.append((v1*fw**l)[0,1])
-        y2.append((v2*fw**l)[0,1])
-
-    return y1, y2
-
-def plot_c(P, x, k):
-    y1, y2 = task_c(P, x, k)
-    print('Length og y1 and y2: ', len(y1), ', ', len(y2))
-    plt.style.use("ggplot")
-    plt.title('Forward and backward propabilities as a function of $l$')
-    plt.ylabel("$P(X_l = 2)$")
-    plt.xlabel("$l$")
-    plt.plot(range(1,51), y1)
-    plt.plot(range(1,51), y2)
-    plt.legend(["$P(X_l = 2 | X_{20} = 1)$", "$P(X_l = 2 | X_{20} = 2)$"])
-    plt.show()
-
-
-def task_d(P, x):
+def task_d():
     '''
     This function gets the probabilities from task a 
     and sums over these with a cost of 5000 per road.
     '''
-    marg_1, marg_2 = task_a(P, x)
-    #print(probabilities)
-    price = 0
-    count = 0
-    for prob in marg_2:
-        #print(prob)
-        price+=5000*prob
-        count+=1
-    print('\ntotal cost = ', price)
+    cost = min([1000000, 5000*sum([P_x(k) for k in range(1,51)])])
+    
+    print('\ntotal cost = ', cost)
     print('optimal choice is: ')
-    if price < 100000:
+    if cost < 100000:
         print('5000 per road ')
-        return price
     else:
         print('fixed price of 100000')
-        return 100000
-
-#task_d(P, x)
-'''
-np.set_printoptions(precision=3)
-print('start')
-a1, a2 = task_c(P,x,10)
-print(a1, '\n', a2)
-a1, a2 = task_c(P,x,40)
-print('\n\n',a1, '\n', a2)
-'''
-def inf_gain_at_k(P, x, k):
-    FIX = 100000
-    y1, y2 = task_c(P, x, k)
-    #print(y1, y2)
-    marg_1, marg_2 = task_a(P, x)
-    s1 = 0
-    s2 = 0
-    for n in range(0, 50):
-        s1 += 5000*y1[n]
-        s2 += 5000*y2[n]
-
-    #print("s1: {} -> {}, s2: {} -> {}".format(s1, min([FIX, s1]), s2, min([FIX, s2])))
-    
-    total = min([FIX, s1])*marginal_prob(k) + min([FIX, s2])*(1-marginal_prob(k))
-    #print(total)
-
-    return total
 
 
-def task_e(P, x):
-    totals = []
-    for k in range(50):
-        totals.append(inf_gain_at_k(P, x, k))
-
-    print(totals)
-    print(len(totals))
+def task_e():
     plt.figure()
-    plt.plot(range(1,51), totals)
+    plt.plot(range(1,51), [information_gain(k) for k in range(1,51)])
+    plt.title("$V_k = \sum_{i=1}^{2} min \{1000000, 5000\sum_{n=1}^{50}P(X_n = 2 | X_k = i)\}P(X_k=i)$")
+    plt.xlabel("$k$")
+    plt.ylabel("$V_k$")
     plt.show()
-
-task_e(P, x)
+    
+if __name__ == "__main__":
+    task_a()
+    task_b()
+    task_d()
+    task_e()
